6f6e6b1c86dc39ae420d1d06c6423882979aa492
George Gore <georgelgore@gmail.com>
2017-09-26 20:30:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,7 @@ def oxford_comma(array)
     return array.first
   elsif array.count == 2
     return "#{array[0]} and #{array[1]}"
+  elsif array.count == 3
+    return "#{array[0]} and #{array[1]}, and #{array[2]}"
   end
 end
