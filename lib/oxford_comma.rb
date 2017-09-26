7b9aff3395897294517c5f5f39c8191088a6228c
George Gore <georgelgore@gmail.com>
2017-09-26 20:40:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   elsif array.count == 3
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
   else
-    first_half == array[0...-1].join(", ")
+    first_half = array[0...-1].join(", ")
     first_half << "and #{array[-1]}"
   end
 end
