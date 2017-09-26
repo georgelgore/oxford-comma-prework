f7ad6c4649f8558b80feb6187185cd2e734284b9
George Gore <georgelgore@gmail.com>
2017-09-26 20:31:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,9 @@ def oxford_comma(array)
   elsif array.count == 2
     return "#{array[0]} and #{array[1]}"
   elsif array.count == 3
-    return "#{array[0]} and #{array[1]}, and #{array[2]}"
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    output = []
+    array.map{|el|}
   end
 end
