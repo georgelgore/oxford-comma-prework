17ddf40b0d5f0a448a4c880e204aad732b7fb4c2
George Gore <georgelgore@gmail.com>
2017-09-26 20:41:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,6 +7,6 @@ def oxford_comma(array)
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
   else
     first_half = array[0...-1].join(", ")
-    first_half << ",and #{array[-1]}"
+    first_half << ", and #{array[-1]}"
   end
 end
